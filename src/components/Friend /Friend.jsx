fc2d437130f25dd78740082689b4ae8d657c19f2
princeRubel1 <mdrubel29879@gmail.com>
2023-12-03 15:07:51 +0530

    first commit

---
+++ src/components/Friend /Friend.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Friend.css';
+
+const Friend = ({friend}) => {
+    const {email,id,name,phone} = friend;
+    return (
+        <div className='friend'>
+            <h3>{name}</h3>
+            <p>email:{email}</p>
+            <p>Phone:{phone}</p>
+        </div>
+    );
+};
+
+export default Friend;
